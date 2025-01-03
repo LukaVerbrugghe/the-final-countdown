635ce4a1af629b252e31f347ddae78cb61d63162
professorVerbrugghe <lotrdetailsys@gmail.com>
2025-01-03 13:38:20 +0100

    Finished

--- prjFinalCountdown/prjFinalCountdown/Form1.Designer.cs
+++ prjFinalCountdown/prjFinalCountdown/Form1.Designer.cs
@@ -28,24 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblLAbel = new System.Windows.Forms.Label();
+            this.lblHoursMinutesSeconds = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTotalSeconds = new System.Windows.Forms.Label();
+            this.lblTotalMinutes = new System.Windows.Forms.Label();
+            this.lblTotalHours = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblLAbel
             // 
             this.lblLAbel.AutoSize = true;
             this.lblLAbel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLAbel.Location = new System.Drawing.Point(109, 106);
+            this.lblLAbel.Location = new System.Drawing.Point(12, 9);
             this.lblLAbel.Name = "lblLAbel";
             this.lblLAbel.Size = new System.Drawing.Size(92, 32);
             this.lblLAbel.TabIndex = 0;
             this.lblLAbel.Text = "label1";
             // 
+            // lblHoursMinutesSeconds
+            // 
+            this.lblHoursMinutesSeconds.AutoSize = true;
+            this.lblHoursMinutesSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoursMinutesSeconds.Location = new System.Drawing.Point(12, 41);
+            this.lblHoursMinutesSeconds.Name = "lblHoursMinutesSeconds";
+            this.lblHoursMinutesSeconds.Size = new System.Drawing.Size(92, 32);
+            this.lblHoursMinutesSeconds.TabIndex = 0;
+            this.lblHoursMinutesSeconds.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTotalSeconds
+            // 
+            this.lblTotalSeconds.AutoSize = true;
+            this.lblTotalSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSeconds.Location = new System.Drawing.Point(12, 90);
+            this.lblTotalSeconds.Name = "lblTotalSeconds";
+            this.lblTotalSeconds.Size = new System.Drawing.Size(92, 32);
+            this.lblTotalSeconds.TabIndex = 0;
+            this.lblTotalSeconds.Text = "label1";
+            // 
+            // lblTotalMinutes
+            // 
+            this.lblTotalMinutes.AutoSize = true;
+            this.lblTotalMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalMinutes.Location = new System.Drawing.Point(12, 122);
+            this.lblTotalMinutes.Name = "lblTotalMinutes";
+            this.lblTotalMinutes.Size = new System.Drawing.Size(92, 32);
+            this.lblTotalMinutes.TabIndex = 0;
+            this.lblTotalMinutes.Text = "label1";
+            // 
+            // lblTotalHours
+            // 
+            this.lblTotalHours.AutoSize = true;
+            this.lblTotalHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalHours.Location = new System.Drawing.Point(12, 154);
+            this.lblTotalHours.Name = "lblTotalHours";
+            this.lblTotalHours.Size = new System.Drawing.Size(92, 32);
+            this.lblTotalHours.TabIndex = 0;
+            this.lblTotalHours.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(634, 201);
+            this.Controls.Add(this.lblTotalHours);
+            this.Controls.Add(this.lblTotalMinutes);
+            this.Controls.Add(this.lblTotalSeconds);
+            this.Controls.Add(this.lblHoursMinutesSeconds);
             this.Controls.Add(this.lblLAbel);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +112,11 @@
         #endregion
 
         private System.Windows.Forms.Label lblLAbel;
+        private System.Windows.Forms.Label lblHoursMinutesSeconds;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTotalSeconds;
+        private System.Windows.Forms.Label lblTotalMinutes;
+        private System.Windows.Forms.Label lblTotalHours;
     }
 }
 
--- prjFinalCountdown/prjFinalCountdown/Form1.cs
+++ prjFinalCountdown/prjFinalCountdown/Form1.cs
@@ -15,7 +15,27 @@ namespace prjFinalCountdown
         public Form1()
         {
             InitializeComponent();
+            timer1.Start();
+        }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //calculate the time until the end of the year
+            DateTime today = DateTime.Now;
+            DateTime endOfYear = new DateTime(today.Year, 6, 30);
+            TimeSpan timeUntilEndOfYear = endOfYear - today;
+            //display the time until the end of the year
+            lblLAbel.Text = "There are " + timeUntilEndOfYear.Days + " days left in this schoolyear.";
+
+            //display as countdown
+            lblHoursMinutesSeconds.Text = "That is " + timeUntilEndOfYear.Days.ToString() + " days and " + timeUntilEndOfYear.Hours.ToString() + ":" + timeUntilEndOfYear.Minutes.ToString() + ":" + timeUntilEndOfYear.Seconds.ToString();
+
+            //display total number of seconds
+            lblTotalSeconds.Text = "That is " + Math.Floor(timeUntilEndOfYear.TotalSeconds).ToString() + " seconds";
+            //display total number of minutes
+            lblTotalMinutes.Text = "That is " + Math.Floor(timeUntilEndOfYear.TotalMinutes).ToString() + " minutes";
+            //dispay total number of hours
+            lblTotalHours.Text = "That is " + Math.Floor(timeUntilEndOfYear.TotalHours).ToString() + " hours";
         }
     }
 }
