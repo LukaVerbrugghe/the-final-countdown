7274dc4f33013daa54629e569ba5b11649fd665b
professorVerbrugghe <lotrdetailsys@gmail.com>
2025-01-03 13:17:27 +0100

    Create empty project

---
+++ prjFinalCountdown/prjFinalCountdown/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace prjFinalCountdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLAbel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLAbel
+            // 
+            this.lblLAbel.AutoSize = true;
+            this.lblLAbel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLAbel.Location = new System.Drawing.Point(109, 106);
+            this.lblLAbel.Name = "lblLAbel";
+            this.lblLAbel.Size = new System.Drawing.Size(92, 32);
+            this.lblLAbel.TabIndex = 0;
+            this.lblLAbel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblLAbel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLAbel;
+    }
+}
+
